d5369c23d6f3d641eedb35d097d55c96b23f4ced
Astrid Cely <sonia.cely@uptc.edu.co>
2022-07-10 01:13:11 -0500

    se añadio otra seccion donde se encuentran tarjetas informativas y una seccion mas abajo de videojuegos

--- src/pages/Home/views/HomePage.jsx
+++ src/pages/Home/views/HomePage.jsx
@@ -1,5 +1,4 @@
-import image from '../../../assets/img/header-zona-gamer.png'
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   return (
@@ -9,7 +8,8 @@ export const HomePage = () => {
           <div
             className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
-              backgroundImage:"image",
+              backgroundImage:
+                "url('https://raw.githubusercontent.com/AstridCely/proyecto-trabajo-de-campo-frontend/main/src/assets/img/header-zona-gamer.png')",
             }}
           >
             <span
@@ -25,45 +25,31 @@ export const HomePage = () => {
                     Zona Gamer
                   </h1>
                   <p className="mt-4 text-lg text-gray-200">
-                   Tempor ut sit minim irure nulla. Anim occaecat id reprehenderit voluptate ut ut nisi nulla cillum ut aliqua mollit. Reprehenderit commodo mollit irure cupidatat. Consectetur voluptate proident dolore non ipsum Lorem mollit adipisicing.
+                    Tempor ut sit minim irure nulla. Anim occaecat id
+                    reprehenderit voluptate ut ut nisi nulla cillum ut aliqua
+                    mollit. Reprehenderit commodo mollit irure cupidatat.
+                    Consectetur voluptate proident dolore non ipsum Lorem mollit
+                    adipisicing.
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-            style={{ transform: "translateZ(0)" }}
-          >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-blueGray-200 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
-          </div>
         </div>
-        {/* <section className="pb-20 bg-blueGray-200 -mt-24">
-          <div className="container mx-auto px-4">
+        <section className="pb-20 bg-gray-200 -mt-24">
+          <div className="container mx-auto px-4 lg:px-20">
             <div className="flex flex-wrap">
               <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                      <i className="fas fa-award"></i>
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-500">
+                      <i className="fas fa-clock"></i>
                     </div>
-                    <h6 className="text-xl font-semibold">Awarded Agency</h6>
+                    <h6 className="text-xl font-semibold">Tiempo</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
-                      Divide details about your product or agency work into
-                      parts. A paragraph describing a feature will be enough.
+                      Reprehenderit commodo mollit irure cupidatat. Consectetur
+                      voluptate proident dolore non ipsum Lorem mollit
+                      adipisicing.
                     </p>
                   </div>
                 </div>
@@ -72,13 +58,14 @@ export const HomePage = () => {
               <div className="w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                      <i className="fas fa-retweet"></i>
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                      <i className="fas fa-desktop"></i>
                     </div>
-                    <h6 className="text-xl font-semibold">Free Revisions</h6>
+                    <h6 className="text-xl font-semibold">Dispositivos</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
-                      Keep you user engaged by providing meaningful information.
-                      Remember that by this time, the user is curious.
+                      Reprehenderit commodo mollit irure cupidatat. Consectetur
+                      voluptate proident dolore non ipsum Lorem mollit
+                      adipisicing.
                     </p>
                   </div>
                 </div>
@@ -87,13 +74,14 @@ export const HomePage = () => {
               <div className="pt-6 w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
-                      <i className="fas fa-fingerprint"></i>
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                      <i className="fas fa-dollar-sign"></i>
                     </div>
-                    <h6 className="text-xl font-semibold">Verified Company</h6>
+                    <h6 className="text-xl font-semibold">Dinero</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
-                      Write a few lines about each one. A paragraph describing a
-                      feature will be enough. Keep you user engaged!
+                      Reprehenderit commodo mollit irure cupidatat. Consectetur
+                      voluptate proident dolore non ipsum Lorem mollit
+                      adipisicing.
                     </p>
                   </div>
                 </div>
@@ -103,32 +91,26 @@ export const HomePage = () => {
             <div className="flex flex-wrap items-center mt-32">
               <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
                 <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <i className="fas fa-user-friends text-xl"></i>
+                  <i className="fas fa-gamepad text-xl"></i>
                 </div>
                 <h3 className="text-3xl mb-2 font-semibold leading-normal">
-                  Working with us is a pleasure
+                  Gran Variedad de Videojuegos
                 </h3>
                 <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
-                  Don't let your uses guess by attaching tooltips and popoves to
-                  any element. Just make sure you enable them first via
-                  JavaScript.
+                  Reprehenderit commodo mollit irure cupidatat. Consectetur
+                  voluptate proident dolore non ipsum Lorem mollit adipisicing.
                 </p>
                 <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
-                  The kit comes with three pre-built pages to help you get
-                  started faster. You can change the text and images and you're
-                  good to go. Just make sure you enable them first via
-                  JavaScript.
+                  Reprehenderit commodo mollit irure cupidatat. Consectetur
+                  voluptate proident dolore non ipsum Lorem mollit adipisicing.
                 </p>
-                <Link to="/" className="font-bold text-blueGray-700 mt-8">
-                  Check Notus React!
-                </Link>
               </div>
 
               <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
-                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-red-400">
                   <img
-                    alt="..."
-                    src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+                    alt="zona-gamer"
+                    src="https://raw.githubusercontent.com/AstridCely/proyecto-trabajo-de-campo-frontend/main/src/assets/img/card-image.png"
                     className="w-full align-middle rounded-t-lg"
                   />
                   <blockquote className="relative p-8 mb-4">
@@ -140,23 +122,23 @@ export const HomePage = () => {
                     >
                       <polygon
                         points="-30,95 583,95 583,65"
-                        className="text-lightBlue-500 fill-current"
+                        className="text-red-400 fill-current"
                       ></polygon>
                     </svg>
                     <h4 className="text-xl font-bold text-white">
-                      Top Notch Services
+                      Titulo/imagen de prueba
                     </h4>
                     <p className="text-md font-light mt-2 text-white">
-                      The Arctic Ocean freezes every winter and much of the
-                      sea-ice then thaws every summer, and that process will
-                      continue whatever happens.
+                      Reprehenderit commodo mollit irure cupidatat. Consectetur
+                      voluptate proident dolore non ipsum Lorem mollit
+                      adipisicing.
                     </p>
                   </blockquote>
                 </div>
               </div>
             </div>
           </div>
-        </section> */}
+        </section>
       </main>
     </>
   );
